adb31d90bee2f38fb8010d98bf1d53adb03c65ac
Tomdatbenik <tomvankaathoven@gmail.com>
2020-03-26 16:48:56 +0100

    Animation of the player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public Speed speed;
     public Rigidbody2D rb;
-  
+    public Animator animator;
+    private Vector2 movement = new Vector2(0, 0);
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
     private void FixedUpdate()
     {
         Move();
